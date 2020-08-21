8258fef55768b60dbe8b1a102613acdd11ba180a
John Leintz <johnleintz@scaledflow.com>
2020-08-21 15:32:27 -0500

    added unix time converter and stock pattern recognition algo

--- testApi.js
+++ testApi.js
@@ -31,22 +31,62 @@ const finnhubClient = new finnhub.DefaultApi();
 // Stock Price
 request('https://finnhub.io/api/v1/quote?symbol=TSLA&token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
  if (err) { return console.log(err); }
- console.log(body);
+// console.log(body);
 });
 
-// Forex 
-// request('https://finnhub.io/api/v1/forex/symbol?exchange=oanda&token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
-//   if (err) { return console.log(err); }
-//   console.log(body);
-// });
-
+// Forex
 request('https://finnhub.io/api/v1/forex/rates?base=USD&token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
  if (err) { return console.log(err); }
- console.log(body);
+// console.log(body);
+});
+
+// Support Resistance
+request('https://finnhub.io/api/v1/scan/support-resistance?symbol=TSLA&resolution=D&token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+//  console.log(body);
+});
+
+// Pattern Recognition
+request('https://finnhub.io/api/v1/scan/pattern?symbol=TSLA&resolution=D&token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.points[0].atime);
 });
 
 // Covid Data
 request('https://finnhub.io/api/v1/covid19/us?token=bt01m7n48v6ouqftkos0', { json: true }, (err, res, body) => {
  if (err) { return console.log(err); }
- console.log(body[0]);
+// console.log(body[0]);
 });
+
+// Convert Unix Time Stamp
+
+let unix_timestamp = 1595462400
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+console.log("unix convertion: " + formattedTime);
+
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
+  console.log(timeConverter(1595462400));
