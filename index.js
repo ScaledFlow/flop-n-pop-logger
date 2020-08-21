6cf93b1fe610e3ae284d9a31294465aaee994976
John Leintz <johnleintz@scaledflow.com>
2020-08-21 14:11:10 -0500

    added finnhub api test stock price call

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ price.getCryptoPrice("USD", "ETH").then(obj => { // Base for ex - USD, Crypto fo
 }).catch(err => {
   console.log(err)
 })
+
+// // Initialize Finnhub
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = "bt01m7n48v6ouqftkos0"; // Replace this
+const finnhubClient = new finnhub.DefaultApi();
+
+
+request('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token=brvkn6nrh5rd378r3l5g', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body);
+});
